d033673c767ab756b57df8ab5deadb4aee7514fe
Artemijs Belokrilecs <artjombelich@gmail.com>
2024-05-02 20:03:15 +0300

    .

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -5,13 +5,13 @@ function Home()
 {
     return(
         <div className='home__wrap'>
-            <div className='text__area'>
+            <div className='intro__text'>
                 <h1>Ciemakukulis</h1>
                 <p>Garši rausīši, silta maizīte, uz mēles kūstiši cepumiņi, tortes un citi kārumi...</p>
-                <button>Katalogs</button>
             </div>
-            <div className='photo__area'>
-                <img src={HomeImg}></img>
+            <div className='intro__buttons'>
+                <button>Katalogs</button>
+                <button>Lokacijas</button>
             </div>
         </div>
 
